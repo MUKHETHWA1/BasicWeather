8af5f0dbc6313457438c357e78c0ff768b2ca765
MUKHETHWA <masikngl@gmail.com>
2024-08-08 15:04:56 +0000

    Updates

---
+++ app/src/main/java/com/example/basicweather/model/Maximum.kt
@@ -0,0 +1,11 @@
+package com.example.basicweather.model
+import com.google.gson.annotations.SerializedName
+
+
+data class Maximum (
+
+  @SerializedName("Value"    ) var Value    : Int?    = null,
+  @SerializedName("Unit"     ) var Unit     : String? = null,
+  @SerializedName("UnitType" ) var UnitType : Int?    = null
+
+)
---
+++ app/src/main/java/com/example/basicweather/model/Temperature.kt
@@ -0,0 +1,10 @@
+package com.example.basicweather.model
+import com.google.gson.annotations.SerializedName
+
+
+data class Temperature (
+
+  @SerializedName("Minimum" ) var Minimum : Minimum? = Minimum(),
+  @SerializedName("Maximum" ) var Maximum : Maximum? = Maximum()
+
+)
